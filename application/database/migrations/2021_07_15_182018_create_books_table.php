540f8c94366df65aa5cf4cc106a63d66af8aae26
yevgen-clickable <borshch_e@clickable.agency>
2021-07-15 22:31:43 +0300

    make migrations

---
+++ application/database/migrations/2021_07_15_182018_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->integer('category_id');
+            $table->integer('series_id');
+            $table->integer('current_page');
+            $table->string('image');
+            $table->integer('pages');
+            $table->boolean('readed');
+            $table->string('title');
+            $table->string('year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
