8752b6b51b0dc66248d29e01ac0e6ef146300b58
yevgen-clickable <borshch_e@clickable.agency>
2021-07-27 19:01:47 +0300

    add messages for API

--- application/resources/lang/en/api.php
+++ application/resources/lang/en/api.php
@@ -2,4 +2,10 @@
 
 return [
     'arguments.bad' => 'Bad arguments in request',
+    'id.required' => 'Field ID is required',
+    'id.doesntExist' => 'ID not found',
+    'firstname.required' => 'Field firstname is required',
+    'firstname.min' => 'Field firstname should be more than :min symbols',
+    'lastname.required' => 'Field lastname is required',
+    'lastname.min' => 'Field lastname should be more than :min symbols',
 ];
--- application/resources/lang/ru/api.php
+++ application/resources/lang/ru/api.php
@@ -2,4 +2,10 @@
 
 return [
     'arguments.bad' => 'Неправильные агрументы в запросе',
+    'id.required' => 'Поле ID обязательное',
+    'id.doesntExist' => 'ID не найден',
+    'firstname.required' => 'Поле имя обязательное',
+    'firstname.min' => 'Поле имя должно быть более чем :min символов',
+    'lastname.required' => 'Поле фамилия обязательное',
+    'lastname.min' => 'Поле фамилия должно быть более чем :min символов',
 ];
