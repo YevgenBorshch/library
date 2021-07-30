bc6434e70e22149c8ff70073c45a45f19feab0bb
yevgen-clickable <borshch_e@clickable.agency>
2021-07-30 21:54:50 +0300

    add user seed for create test user

--- application/database/seeders/DatabaseSeeder.php
+++ application/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Author;
+use App\Models\Category;
+use App\Models\Series;
+use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +18,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Author::factory()->count(100)->create();
+        Category::factory()->count(20)->create();
+        Series::factory()->count(20)->create();
+        Tag::factory()->count(20)->create();
+
+        $this->call([
+            UserSeeder::class,
+        ]);
     }
 }
