3102b6b21c69a4eca9ed20f6d16b4197192b6c4e
yevgen-clickable <borshch_e@clickable.agency>
2021-08-24 19:55:50 +0300

    change exceptions for api

--- application/app/Exceptions/ApiArgumentException.php
+++ application/app/Exceptions/ApiArgumentException.php
@@ -2,28 +2,44 @@
 
 namespace App\Exceptions;
 
+use App\DTO\ResponseDTO;
+use App\Traits\MakeHashTrait;
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
+use stdClass;
 
 class ApiArgumentException extends Exception
 {
+    use MakeHashTrait;
+
     /**
      * @var string
      */
     protected $message;
 
     /**
-     * ApiAuthException constructor.
-     * @param string $message
+     * @var string
      */
-    public function __construct(string $message)
+    protected string $context;
+
+    /**
+     * @var string
+     */
+    protected string $hash;
+
+    /**
+     * @param string $message
+     * @param string $context
+     */
+    public function __construct(string $message, string $context = '')
     {
         parent::__construct();
         $this->message = $message;
+        $this->context = $context;
+        $this->hash = $this->makeErrorHash();
     }
-
 
     /**
      * Report the exception.
@@ -32,7 +48,7 @@ class ApiArgumentException extends Exception
      */
     public function report(): ?bool
     {
-        Log::error('customError', [$this->message]);
+        Log::error($this->hash, ['error' => $this->message, 'context' => $this->context]);
         return true;
     }
 
@@ -40,10 +56,12 @@ class ApiArgumentException extends Exception
      * Render the exception into an HTTP response.
      *
      * @param  Request  $request
-     * @return Response
+     * @return JsonResponse
      */
-    public function render(Request $request): Response
+    public function render(Request $request): JsonResponse
     {
-        return response(['message' => $this->message], 500);
+        return response()->json(
+            new ResponseDTO( new stdClass(), [$this->message], $this->hash), 404
+        );
     }
 }
--- application/app/Exceptions/ApiAuthException.php
+++ application/app/Exceptions/ApiAuthException.php
@@ -2,26 +2,43 @@
 
 namespace App\Exceptions;
 
+use App\DTO\ResponseDTO;
+use App\Traits\MakeHashTrait;
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
+use stdClass;
 
 class ApiAuthException extends Exception
 {
+    use MakeHashTrait;
+
     /**
      * @var string
      */
     protected $message;
 
     /**
-     * ApiAuthException constructor.
-     * @param string $message
+     * @var string
      */
-    public function __construct(string $message)
+    protected string $context;
+
+    /**
+     * @var string
+     */
+    protected string $hash;
+
+    /**
+     * @param string $message
+     * @param string $context
+     */
+    public function __construct(string $message, string $context = '')
     {
         parent::__construct();
         $this->message = $message;
+        $this->context = $context;
+        $this->hash = $this->makeErrorHash();
     }
 
 
@@ -32,7 +49,7 @@ class ApiAuthException extends Exception
      */
     public function report(): ?bool
     {
-        Log::error('customError', [$this->message]);
+        Log::error($this->hash, ['error' => $this->message, 'context' => $this->context]);
         return true;
     }
 
@@ -40,10 +57,12 @@ class ApiAuthException extends Exception
      * Render the exception into an HTTP response.
      *
      * @param  Request  $request
-     * @return Response
+     * @return JsonResponse
      */
-    public function render(Request $request): Response
+    public function render(Request $request): JsonResponse
     {
-        return response(['message' => $this->message], 500);
+        return response()->json(
+            new ResponseDTO( new stdClass(), [$this->message], $this->hash)
+        );
     }
 }
