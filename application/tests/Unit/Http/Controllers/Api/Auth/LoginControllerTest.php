1b85cd9033341cf6eb82c7ffdeeffe7bfa9ccc40
yevgen-clickable <borshch_e@clickable.agency>
2021-08-24 17:51:29 +0300

    add negative tests

--- application/tests/Unit/Http/Controllers/Api/Auth/LoginControllerTest.php
+++ application/tests/Unit/Http/Controllers/Api/Auth/LoginControllerTest.php
@@ -8,7 +8,7 @@ use Tests\TestCase;
 
 class LoginControllerTest extends TestCase
 {
-    public function testLogin()
+    public function testLoginWithValidData()
     {
         $user = User::factory()->create();
 
@@ -23,5 +23,41 @@ class LoginControllerTest extends TestCase
         $content = json_decode($response->getContent(), true);
 
         $this->assertArrayHasKey('token', $content);
+    }
+
+    public function testLoginWithoutEmail()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post(route('auth.login'), [
+            'password' => "password",
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(422);
+        $content = json_decode($response->getContent(), true);
+
+        $this->arrayHasKey('errors', $content);
+        $this->arrayHasKey('hash', $content);
+        $this->assertCount(1, $content['errors']);
+    }
+
+    public function testLoginWithoutPassword()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post(route('auth.login'), [
+            'email' => $user->email
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(422);
+        $content = json_decode($response->getContent(), true);
+
+        $this->arrayHasKey('errors', $content);
+        $this->arrayHasKey('hash', $content);
+        $this->assertCount(1, $content['errors']);
     }
 }
--- application/tests/Unit/Http/Controllers/Api/Auth/RegistrationControllerTest.php
+++ application/tests/Unit/Http/Controllers/Api/Auth/RegistrationControllerTest.php
@@ -7,7 +7,7 @@ use Tests\TestCase;
 
 class RegistrationControllerTest extends TestCase
 {
-    public function testRegister(): void
+    public function testRegisterWithValidData(): void
     {
         $user = User::factory()->make();
 
@@ -25,5 +25,108 @@ class RegistrationControllerTest extends TestCase
 
         $this->assertEquals($user->name, $content['result']['name']);
         $this->assertEquals($user->email, $content['result']['email']);
+    }
+
+    public function testRegisterWithInvalidPasswordConfirmation()
+    {
+        $user = User::factory()->make();
+
+        $response = $this->post(route('auth.register'), [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => $user->password . '!'
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(422);
+        $content = json_decode($response->getContent(), true);
+
+        $this->arrayHasKey('errors', $content);
+        $this->arrayHasKey('hash', $content);
+        $this->assertCount(1, $content['errors']);
+    }
+
+    public function testRegisterWithEmptyPasswordConfirmation()
+    {
+        $user = User::factory()->make();
+
+        $response = $this->post(route('auth.register'), [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => ''
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(422);
+        $content = json_decode($response->getContent(), true);
+
+        $this->arrayHasKey('errors', $content);
+        $this->arrayHasKey('hash', $content);
+        $this->assertCount(1, $content['errors']);
+    }
+
+    public function testRegisterWithEmptyPassword()
+    {
+        $user = User::factory()->make();
+
+        $response = $this->post(route('auth.register'), [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => '',
+            'password_confirmation' => $user->password
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(422);
+        $content = json_decode($response->getContent(), true);
+
+        $this->arrayHasKey('errors', $content);
+        $this->arrayHasKey('hash', $content);
+        $this->assertCount(1, $content['errors']);
+    }
+
+    public function testRegisterWithoutEmail()
+    {
+        $user = User::factory()->make();
+
+        $response = $this->post(route('auth.register'), [
+            'name' => $user->name,
+            'password' => $user->password,
+            'password_confirmation' => $user->password
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(422);
+        $content = json_decode($response->getContent(), true);
+
+        $this->arrayHasKey('errors', $content);
+        $this->arrayHasKey('hash', $content);
+        $this->assertCount(1, $content['errors']);
+    }
+
+    public function testRegisterWithoutName()
+    {
+        $user = User::factory()->make();
+
+        $response = $this->post(route('auth.register'), [
+            'email' => $user->email,
+            'password' => $user->password,
+            'password_confirmation' => $user->password
+        ], [
+            'Accept' => 'application/json',
+        ]);
+
+        $response->assertStatus(422);
+        $content = json_decode($response->getContent(), true);
+
+        $this->arrayHasKey('errors', $content);
+        $this->arrayHasKey('hash', $content);
+        $this->assertCount(1, $content['errors']);
     }
 }
