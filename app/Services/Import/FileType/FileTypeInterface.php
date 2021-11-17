810bdd9d1807f2cf26a034fe28f1c58238c9cdff
Евгений Борщ <b0980652405@gmail.com>
2021-11-17 21:40:01 +0200

    add file type

---
+++ app/Services/Import/FileType/FileTypeInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services\Import\FileType;
+
+interface FileTypeInterface
+{
+    public function saveBook();
+}
---
+++ app/Services/Import/FileType/PDF.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services\Import\FileType;
+
+class PDF implements FileTypeInterface
+{
+    public function saveBook()
+    {
+
+    }
+}
